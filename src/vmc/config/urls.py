3e8cbe385376a20ee1a08923a3cf3555eaad5192
Michał Walkowski <mi.walkowski@gmail.com>
2020-02-18 11:19:39 +0100

    removed swagger url

--- src/vmc/config/urls.py
+++ src/vmc/config/urls.py
@@ -17,30 +17,10 @@
  * under the License.
  *
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
-
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="VMC API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="vmc-support@dsecure.me"),
-      license=openapi.License(name="Apache License Version 2.0"),
-   ),
-   public=True,
-)
+from django.urls import path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('api-auth/', include('rest_framework.urls'))
 ]
