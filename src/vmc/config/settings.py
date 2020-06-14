a3811626347774289f2d36beb67c0d38dd635d1e
Michał Walkowski <mi.walkowski@gmail.com>
2020-06-14 22:10:44 +0200

    fix ci/cd

--- src/vmc/config/settings.py
+++ src/vmc/config/settings.py
@@ -161,7 +161,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(VMC_PATH, "static"),
 ]
 
 CELERY_BROKER_URL = 'amqp://{}:{}@{}:{}'.format(
